4688502ab6e56517966d6865d9bb71782c03fc72
Sonnieperis <sonnieperis17@gmail.com>
2024-05-25 19:49:13 +0300

    Text

--- text_to_speech_method_2.py
+++ text_to_speech_method_2.py
@@ -9,6 +9,6 @@ language ="en"
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-# engine.say(myText)
+engine.say(myText)
 # engine.runAndWait()
 # ----------------------------------------------------------End------------------------------------------------------
