15c202c949fc3763255ac377ee5f108b687ec945
Sonnieperis <sonnieperis17@gmail.com>
2024-05-29 16:33:37 +0300

    text-to-speech

--- text_to_speech_method_2.py
+++ text_to_speech_method_2.py
@@ -7,7 +7,7 @@ myText=  input("Enter the text\n")
 language ="en"
 
 # engine = pyttsx3.init()
-# voices = engine.getProprty('voices')
+voices = engine.getProprty('voices')
 # engine.setProperty('voice', voices[1].id)
 engine.say(myText)
 engine.runAndWait()
