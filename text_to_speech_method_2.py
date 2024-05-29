fc1bd05096552af694cde553242152a0160e25fe
Sonnieperis <sonnieperis17@gmail.com>
2024-05-29 16:34:31 +0300

    text-to-speech

--- text_to_speech_method_2.py
+++ text_to_speech_method_2.py
@@ -6,7 +6,7 @@ import pyttsx3
 myText=  input("Enter the text\n")
 language ="en"
 
-# engine = pyttsx3.init()
+engine = pyttsx3.init()
 voices = engine.getProprty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.say(myText)
